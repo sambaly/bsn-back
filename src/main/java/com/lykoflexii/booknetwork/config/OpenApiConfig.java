2a202def0109b678996fdc5b5c28f2945467b4a2
sambaly <kote.sambaly@gmail.com>
2024-05-12 15:21:21 +0000

    back end done

---
+++ src/main/java/com/lykoflexii/booknetwork/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package com.lykoflexii.booknetwork.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Sambaly",
+                        email = "contact@sambaly.com",
+                        url = "https://..............com"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi Specification - Sambaly",
+                version = "1.0",
+                license = @License(
+                        name = "License name",
+                        url = "https://................com"
+                ),
+                termsOfService = "Terms Of Service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://sambaly.com"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Auth Description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
